94f54d60e1d214d271b63323fd459980236230ef
Sean Koval <sean@capitalprawn.com>
2021-03-14 23:56:42 -0400

    changes to term

--- sentiment_terminal.py
+++ sentiment_terminal.py
@@ -11,6 +11,11 @@ from pyfiglet import figlet_format
 from alpa_vantage.timeseries import TimeSeries
 from prompt_toolkit.completion import NestedCompleter
 
+from sentilab import feature_flags as ff
+from sentilab.menu import session
+from sentilab.sentiment import sentiment
+from sentilab.fundamental_analysis import fa_menu as fam
+
 try:
     import colorama
     colorama.init()
@@ -72,7 +77,65 @@ def main():
             print_help(ticker, start, interval, is_market_open())
             help_print = False
         
-        if session and 
+        if session and ff.USE_PROMPT_TOOLKIT:
+            as input = session.prompt(f"{get_flair()}?> ", completer=completer)
+        else:
+            as_input = input(f"{get_flair()}> ")
+        
+        # Check if command is empty
+        if not as_input:
+            print("")
+            continue
+        
+        # Parse main command list
+        try:
+            (ns_known_args, l_args) = menu_parser.parse_known_args(as_input.split())
+        
+        except SystemExit:
+            print("The command doesn't exist\n")
+            continue
+
+        b_quit = False
+        if ns_known_args.opt == "help":
+            help_print = True 
+        
+        elif (ns_known_args.opt == "quit") or (ns_known_args.opt == "q"):
+            break
+        
+        elif (ns_known_args.opt == "clear"):
+            ticker, start, interval, df_stock = clear(
+                l_args, ticker, start, interval, df_stock
+            )
+            main_command = True 
+            
+        elif ns_known_args.opt == "load":
+            ticker, start, interval, df_stock = load(
+                l_args, ticker, start, interval, df_stock
+            )
+            main_command = True 
+            
+        
+        elif ns_known_args.opt == "view":
+            view(l_args, ticker, start, interval, df_stock)
+            main_command = True 
+        
+        elif ns_known_args.opt == "sentiment":
+            b_quit = sm.sentiment_menu(ticker, start)
+        
+        
+        else:
+            print("Should not get to this command")
+            continue
+        
+        if b_quit:
+            break
+        else:
+            if not main_command:
+                help_print = True 
+        
+        
+    print("Exiting sentilab Terminal.\n")
+
 
 
 
